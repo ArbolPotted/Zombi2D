3b1f3b6928f803fcfb476025c7748b54c5b8956f
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-11-10 18:54:49 +0100

    03_disparar bala

---
+++ Assets/Scripts/balaScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balaScript : MonoBehaviour {
+
+	public Vector2 velocity = mew Vector2(5,0);
+
+	void Start () {
+				Rigidbody2D.velocity = AudioVelocityUpdateMode * Transform.localScale.x;
+	
+		}
+
+}
--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -2,37 +2,64 @@
 using System.Collections;
 
 public class playerScript : MonoBehaviour {
-
 	public float speed = 10f;
 	public Vector2 maxVelocity = new Vector2(2,3);
-	//private Animator animator;
-
-
+	private Animator animator;
+	
 	// Use this for initialization
 	void Start () {
-
-		//animator = GetComponent<Animator> ();
-	
+		
+		animator = GetComponent <Animator> ();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		var absVelX = Mathf.Abs (rigidbody2D.velocity.x); //establecer la velocidad (velocidad absoluta de eje X)
+		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
 		var forceX = 0f;
-		var forceY = 0f; //fuerza que le doy al personaje
-
-		//Calcular velocityX
-		if (Input.GetKey ("right")) {
-						if (absVelX < maxVelocity.x)
-								forceX = speed;
-						this.transform.localScale = new Vector3 (1, 1, 1);
-				} else if (Input.GetKey ("left")) {
-						if (absVelX < maxVelocity.x)
-								forceX = -speed;
-						this.transform.localScale = new Vector3 (-1, 1, 1);//para que gire el personaje
-				}
-		rigidbody2D.AddForce(new Vector2(forceX, forceY));
-	
+		var forceY = 0f;
+		
+		//Calculate Velocity X
+		if (Input.GetKey("right"))
+		{
+			//Esto lo que hace es frenar cuando voy a la izquierda y pulso la derecha
+			if (rigidbody2D.velocity.x < 0) {
+				rigidbody2D.velocity = new Vector2 (0, rigidbody2D.velocity.y);
+			}
+			
+			if(absVelX < maxVelocity.x)
+				forceX = speed;
+			//Esto pone el sprite mirando a la derecha
+			this.transform.localScale = new Vector3 (1,1,1);
+		}
+		else if (Input.GetKey("left"))
+		{
+			//Esto lo que hace es frenar cuando voy a la derecha y pulso la izquierda
+			if (rigidbody2D.velocity.x > 0) {
+				rigidbody2D.velocity = new Vector2 (0, rigidbody2D.velocity.y);
+			}
+			if (absVelX < maxVelocity.x)
+				forceX = -speed;
+			//Esto pone el script mirando a la izquierda
+			this.transform.localScale = new Vector3 (-1,1,1);
+			
+		}
+		//Actualizamos la variable Velocity cuando la velocidad es mayot de 0
+		if (absVelX > 0)
+			animator.SetFloat ("Velocity", absVelX);
+		
+		rigidbody2D.AddForce (new Vector2 (forceX, forceY));
+		
+		
+		if (Input.GetKey ("c")) {
+			animator.SetBool ("Fire", true);
+			
+		} else {
+			animator.SetBool ("Fire", false);
+		}
+		
+		
+		
+		
 	}
 }
