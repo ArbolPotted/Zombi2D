0e36cf4afc1353c320058ab9a2acc8141a628d82
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-11-12 18:01:20 +0100

    04_bala

--- Assets/Scripts/balaScript.cs
+++ Assets/Scripts/balaScript.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class balaScript : MonoBehaviour {
 
-	public Vector2 velocity = mew Vector2(5,0);
+	public Vector2 velocity = new Vector2(5,0);
+	public GameObject particulas;
+	//public GameObject blood;
+	//public int damage = 5;
 
 	void Start () {
-				Rigidbody2D.velocity = AudioVelocityUpdateMode * Transform.localScale.x;
-	
+				//rigidbody2D.velocity = velocity * transform.localScale.x;
+				rigidbody2D.AddForce (velocity * transform.localScale.x, ForceMode2D.Impulse);
+				//Invoke ("onDestroy", 0.2f);
 		}
+		void OnCollisionEnter2D(Collision2D target){
+		Destroy (gameObject);
+		//onDestroy();
 
-}
+		}
+	
+	}
+
