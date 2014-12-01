42d9ef1e070e04c42ae00dfe5aaabaa60f4d544e
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-12-01 18:46:22 +0100

    mas nivel

--- Assets/Scripts/hurtScript.cs
+++ Assets/Scripts/hurtScript.cs
@@ -6,26 +6,37 @@ public class hurtScript : MonoBehaviour {
 	public int damage = 5;
 	public float force = 0;
 	public bool destroyOnCollision = false;
+	public bool destroyOnTrigger = true;
 	
 	private damageSystem ds;
+
 	
 	void OnCollisionStay2D(Collision2D target){
-		
-		if (target.transform.tag == tagDamage) {
-			Debug.Log (target.transform.tag);
-			ds = target.transform.GetComponent<damageSystem>();
-			if(ds){
-				Debug.Log(tagDamage + ":"+damage);
-				ds.hurt(damage);
-				if(force > 0){
-					var direction = target.transform.position - transform.position  ;
-					ds.rigidbody2D.AddForce(direction.normalized * force/10,ForceMode2D.Impulse);
-				}
-			}
-		}
-		
+		herir (target.transform);
 		if (destroyOnCollision) {
 			Destroy(gameObject);
 		}
+
 	}
+
+	void OnTriggerStay2D(Collider2D target){
+		herir (target.transform);
+		if (destroyOnTrigger) {
+			Destroy(gameObject);
+		}
+	}
+
+	void herir(Transform target){
+		if (target.transform.tag == tagDamage) {
+			Debug.Log (target.transform.tag);
+			ds = target.transform.GetComponent<damageSystem> ();
+			ds.hurt(damage);
+			if(force > 0) {
+				var direction = target.transform.position - transform.position;
+				ds.rigidbody2D.AddForce (direction.normalized * force / 10, ForceMode2D.Impulse);
+			}
+
+		}
+			
+}
 }
