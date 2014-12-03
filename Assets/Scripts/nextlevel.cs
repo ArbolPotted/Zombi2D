bbad7c8ffb400b2eef89e6fcf696a666fc1eddb7
ArbolPotted <teresahernandezpizarro@gmail.com>
2014-12-03 18:56:20 +0100

    puerta

---
+++ Assets/Scripts/nextlevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextlevel : MonoBehaviour {
+
+	public string nombreNivel; //nombre escena que vamos a cargar
+	public float tiempoespera = 3f;
+	bool cargando = false; //player esta dentro del trigger
+
+	void OnTriggerStay2D (Collider2D target){
+		if (target.transform.tag == "Player") {
+			//Application.LoadLevel (nombreNivel);
+			if(!cargando)
+				StartCoroutine(cargaNivel());
+		}
+	}
+
+	void OntriggerExit2D(Collider2D target) {
+				cargando = false;
+		}
+	void OnTriggerEnter2D(Collider target) {
+				cargando = true;
+		}
+	IEnumerator cargaNivel() {
+				cargando = true;
+				yield return new WaitForSeconds (tiempoespera);
+				if (cargando)
+						Application.LoadLevel (nombreNivel);
+		}
+
+}
---
+++ Assets/Scripts/puertaScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class puertaScript : MonoBehaviour {
+
+	public Transform destino;
+	public bool delantepuerta = false;
+	public bool teletransporta = false;
+
+	void OnTriggerEnter2D(Collider2D target) {
+				if (target.transform.tag == "Player") {
+						delantepuerta = true;
+				}
+		}
+
+	void OnDrawGizmos() {
+				if (destino != null) {
+						Gizmos.color = Color.blue;
+						Gizmos.DrawLine (transform.position, destino.position);
+				}
+		}
+
+	// Use this for initialization
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && delantepuerta) {
+			teletransporta = true;
+	
+	}
+}
+
+void OnTriggerStay2D (Collider2D target){
+	if (teletransporta) {
+		target.transform.position = destino.position;
+		teletransporta = false;
+			delantepuerta = false;
+
+	}
+}
+	void OnTriggerExit2D(Collider2D target){
+		if (target.transform.tag == "Player")
+			delantepuerta= false;
+	}
+}
